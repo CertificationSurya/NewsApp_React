e2442cbd67b30df30253e78ead4d88743d6e482b
Surya Bahadur Basnet <certification.surya@gmail.com>
2023-07-10 00:30:13 +0545

    added fetching and previous, next Button

--- src/components/News.js
+++ src/components/News.js
@@ -1,19 +1,59 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
-// 0582ab78767848e8ac4338e127c25450
-// https://newsapi.org/v2/everything?q={topic}&apiKey=API_KEY
-
 export default class News extends Component {
+
     // creating a constructor
-    constructor(){
+    constructor() {
 
         super(); // calling the constructor of Component(parent class)
-        this.state ={
-            articles: this.articles,
-            loading: false
+        this.state = {
+            articles: [],
+            loading: false,
+            page: 1,
+        }
+        
+    }
+
+    // Called immediately after a component is mounted. Setting state here will trigger re-rendering.
+    // componentDidMount function executes ASAP but only after render() gets executed
+    async componentDidMount() {
+        let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=0582ab78767848e8ac4338e127c25450&page=1&pageSize=20";
+        let data = await fetch(url);
+        let parsedData = await data.json()
+        this.setState({articles: parsedData.articles, totalResults: parsedData.totalResults})
+
+    }
+
+    handlePreviousClick = async () => {
+        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=0582ab78767848e8ac4338e127c25450&page=${this.state.page - 1}&pageSize=20`;
+        let data = await fetch(url);
+        let parsedData = await data.json()
+        console.log(parsedData);  
+        this.setState({
+            page: this.state.page - 1,
+            articles: parsedData.articles
+        })
+
+    }
+
+    handleNextClick = async () => {
+
+        if (this.state.page + 1 > Math.ceil(this.state.totalResults / 20)) {
+
+        }
+        else {
+            let url = `https://newsapi.org/v2/top-headlines?country=us&apikey=0582ab78767848e8ac4338e127c25450&pages=${this.state.page + 1}&pageSize=20`;
+            let data = await fetch(url);
+            let parsedData = await data.json()
+
+            this.setState({
+                page: this.state.page + 1,
+                articles: parsedData.articles
+            })
         }
     }
+
 
 
 
@@ -21,18 +61,19 @@ export default class News extends Component {
         return (
             <div className='container m-3'>
                 <h2>Top Headlines from all Around the world</h2>
-
                 <div className="row">
-                    <div className="col-md-4">
-                        <NewsItem />
-                    </div>
-                    <div className="col-md-4">
-                        <NewsItem />
-                    </div>
-                    <div className="col-md-4">
-                        <NewsItem />
-                    </div>
 
+                    {this.state.articles.map((element) => {
+                        return <div className="col-md-4" key={element.url}>
+                            <NewsItem title={element.title ? element.title.slice(0, 45) : ""} description={element.description ? element.description.slice(0, 90) : ""} imageUrl={element.urlToImage} newsUrl={element.url} />
+                        </div>
+
+                    })}
+                </div>
+
+                <div className="container d-flex justify-content-evenly">
+                    <button className='btn btn-dark' disabled={this.state.page <= 1} onClick={this.handlePreviousClick}>&larr; Previous</button>
+                    <button className='btn btn-dark' disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / 20)}onClick={this.handleNextClick} >Next &rarr;</button>
                 </div>
             </div>
         )
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -7,11 +7,11 @@ export default class NewsItem extends Component {
         return (
             <div className='m-3'>
                 <div className="card" style={{width: "18rem"}}>
-                    <img src={imageUrl} className="card-img-top" alt="..."/>
+                    <img src={imageUrl? imageUrl :"https://a.espncdn.com/combiner/i?img=%2Fphoto%2F2023%2F0708%2Fr1195390_1296x729_16%2D9.jpg"} className="card-img-top" alt="..."/>
                         <div className="card-body">
-                            <h5 className="card-title">{title}</h5>
-                            <p className="card-text">{description}</p>
-                            <a href="/newsdetails" className="btn btn-sm btn-primary">Read More</a>
+                            <h5 className="card-title">{title}...</h5>
+                            <p className="card-text">{description}...</p>
+                            <a href={newsUrl} target='_blank' className="btn btn-sm btn-dark">Read More</a>
                         </div>
                 </div>
             </div>
